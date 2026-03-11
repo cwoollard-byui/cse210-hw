8df23e4b2e67ec3f0d014ee068362c38cfaf6e5c
cwoollard-byui <231574909+cwoollard-byui@users.noreply.github.com>
2026-03-11 21:23:32 +0000

    W01 Assignment - Move function definitions outside of Main function

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,31 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        static void DisplayWelcome()
-        {
-            Console.WriteLine("Welcome to the Program!");
-        }
-        static string PromptUserName()
-        {
-            Console.Write("Please enter your name: ");
-            string userName = Console.ReadLine();
-            return userName;
-        }
-        static int PromptUserNumber()
-        {
-            Console.Write("Please enter your favorite number: ");
-            int userNumber = int.Parse(Console.ReadLine());
-            return userNumber;
-        }
-        static int SquareNumber(int userNumber)
-        {
-            int square = userNumber * userNumber;
-            return square;
-        }
-        static void DisplayResult(string userName, int square)
-        {
-            Console.WriteLine($"{userName}, the square of your number is {square}");
-        }
 
         DisplayWelcome();
         string userName = PromptUserName();
@@ -36,4 +11,31 @@ class Program
         int square = SquareNumber(userNumber);
         DisplayResult(userName, square);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+    static int SquareNumber(int userNumber)
+    {
+        int square = userNumber * userNumber;
+        return square;
+    }
+    static void DisplayResult(string userName, int square)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {square}");
+    }
+
 }
