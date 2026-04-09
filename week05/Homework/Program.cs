6f59806d9b1d8a50f73a961488ed89c9fe763e59
cwoollard-byui <231574909+cwoollard-byui@users.noreply.github.com>
2026-04-09 22:22:50 +0100

    W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Nigel Smith", "Photosynthesis");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Beatrice Jones", "Quadratic Equations", "12.4", "3-27");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Alistair Cameron", "Norse Mythology", "The Twilight of the Gods: Ragnarok in Modern Literature");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
